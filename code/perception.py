b0e0f2172ad3d310f5f073dbded7dbc12a5ed14d
fl1ghtl0ger <fl1ghtl0ger@gmail.com>
2018-02-19 00:24:55 +0300

    implementation completed

--- code/perception.py
+++ code/perception.py
@@ -74,12 +74,76 @@ def perspect_transform(img, src, dst):
            
     M = cv2.getPerspectiveTransform(src, dst)
     warped = cv2.warpPerspective(img, M, (img.shape[1], img.shape[0]))# keep same size as input image
-    
-    return warped
+    mask = cv2.warpPerspective(np.ones_like(img[:, :, 0]), M, (img.shape[1], img.shape[0]))
+    return warped, mask
+
+
+def find_rocks(img, levels=(110, 110, 50)):
+    rockpix = ((img[:, :, 0] > levels[0]) \
+               & (img[:, :, 1] > levels[1]) \
+               & (img[:, :, 2] < levels[2]))
+
+    color_select = np.zeros_like(img[:, :, 0])
+    color_select[rockpix] = 1
+
+    return color_select
 
 
 # Apply the above functions in succession and update the Rover state accordingly
 def perception_step(Rover):
+
+    dst_size = 5
+    bottom_offset = 6
+    image = Rover.img
+    source = np.float32([[14, 140], [301, 140], [200, 96], [118, 96]])
+    destination = np.float32([[image.shape[1] / 2 - dst_size, image.shape[0] - bottom_offset],
+                              [image.shape[1] / 2 + dst_size, image.shape[0] - bottom_offset],
+                              [image.shape[1] / 2 + dst_size, image.shape[0] - 2 * dst_size - bottom_offset],
+                              [image.shape[1] / 2 - dst_size, image.shape[0] - 2 * dst_size - bottom_offset],
+                              ])
+    warped, mask = perspect_transform(image, source, destination)
+
+    threshed = color_thresh(warped)
+    obs_map = np.absolute(np.float32(threshed) - 1) * mask
+
+    Rover.vision_image[:,:,2] = threshed * 255
+    Rover.vision_image[:,:,0] = obs_map * 255
+
+    xpix, ypix = rover_coords(threshed)
+
+    world_size = Rover.worldmap.shape[0]
+
+    scale = 2 * dst_size
+    x_world, y_world = pix_to_world(xpix, ypix, Rover.pos[0], Rover.pos[1], Rover.yaw, world_size, scale)
+
+    obsxpix, obsypix = rover_coords(obs_map)
+    obs_x_world, obs_y_world = pix_to_world(obsxpix, obsypix, Rover.pos[0], Rover.pos[1], Rover.yaw, world_size, scale)
+
+    Rover.worldmap[y_world, x_world, 2] += 10
+    Rover.worldmap[obs_y_world, obs_x_world, 0] += 1
+
+    dist, angles = to_polar_coords(xpix, ypix)
+
+    Rover.nav_angles = angles
+
+    rock_map = find_rocks((warped * 255), levels=(110, 110, 50))
+    if rock_map.any():
+        rock_x, rock_y = rover_coords(rock_map)
+        rock_x_world, rock_y_world = pix_to_world(rock_x, rock_y, Rover.pos[0], \
+                                                  Rover.pos[1], Rover.yaw, world_size, scale)
+
+        rock_dist, rock_angles = to_polar_coords(rock_x, rock_y)
+        rock_idx = np.argmin(rock_dist)
+        rock_xcen = rock_x_world[rock_idx]
+        rock_ycen = rock_y_world[rock_idx]
+
+        Rover.worldmap[rock_xcen, rock_ycen, 1] = 255
+        Rover.vision_image[:,:,1] = rock_map * 255
+    else:
+        Rover.vision_image[:, :, 1] = 0
+
+    return Rover
+
     # Perform perception steps to update Rover()
     # TODO: 
     # NOTE: camera image is coming to you in Rover.img
@@ -106,4 +170,65 @@ def perception_step(Rover):
  
     
     
-    return Rover
+    # return Rover
+
+
+'''
+def process_image(img):
+    warped, masked = perspect_transform(img, source, destination)
+    threshed = color_thresh(warped)
+    obs_map = np.absolute(np.float32(threshed) -1) 
+    x_pix, y_pix = rover_coords(threshed)
+    
+    world_size = data.worldmap.shape[0]
+    scale = 2 * dst_size
+    xpos = data.xpos[data.count]
+    ypos = data.ypos[data.count]
+    yaw = data.yaw[data.count]
+    x_world, y_world = pix_to_world(xpix, ypix, xpos, ypos, yaw, world_size, scale)
+    
+    obsxpix, obsypix = rover_coords(obs_map)
+    obs_x_world, obs_y_world = pix_to_world(obsxpix, obsypix, xpos, ypos, 
+                                            yaw, world_size, scale)
+    
+    
+    data.worldmap[y_world, x_world, 2] = 255
+    data.worldmap[obs_y_world, obs_x_world, 0] = 255
+    nav_pix = data.worldmap[:,:,2] > 0
+    
+    data.worldmap[nav_pix, 0] = 0
+    
+    rock_map = find_rocks(warped, levels=(110, 110, 50))
+    if rock_map.any():
+        rock_x, rock_y = rover_coords(rock_map)
+        rock_x_world, rock_y_world = pix_to_world(rock_x, rock_y, xpos, \
+                                                  ypos, yaw, world_size, scale)
+        data.worldmap[rock_y_world, rock_x_world, :] = 255
+    
+    # 7) Make a mosaic image, below is some example code
+        # First create a blank image (can be whatever shape you like)
+    output_image = np.zeros((img.shape[0] + data.worldmap.shape[0], img.shape[1]*2, 3))
+        # Next you can populate regions of the image with various output
+        # Here I'm putting the original image in the upper left hand corner
+    output_image[0:img.shape[0], 0:img.shape[1]] = img
+
+        # Let's create more images to add to the mosaic, first a warped image
+    #warped, masked = perspect_transform(img, source, destination)
+        # Add the warped image in the upper right hand corner
+    output_image[0:img.shape[0], img.shape[1]:] = warped
+
+        # Overlay worldmap with ground truth map
+    map_add = cv2.addWeighted(data.worldmap, 1, data.ground_truth, 0.5, 0)
+        # Flip map overlay so y-axis points upward and add to output_image 
+    output_image[img.shape[0]:, 0:data.worldmap.shape[1]] = np.flipud(map_add)
+
+
+        # Then putting some text over the image
+    cv2.putText(output_image,"Populate this image with your analyses to make a video!", (20, 20), 
+                cv2.FONT_HERSHEY_COMPLEX, 0.4, (255, 255, 255), 1)
+    if data.count < len(data.images) - 1:
+        data.count += 1 # Keep track of the index in the Databucket()
+    
+    return output_image
+
+'''
